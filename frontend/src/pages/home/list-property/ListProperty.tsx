90c1c6fab36fa73d4224bdee32b8cab4c2c3baab
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-07 15:01:29 +0100

    added card implementation

--- frontend/src/pages/home/list-property/ListProperty.tsx
+++ frontend/src/pages/home/list-property/ListProperty.tsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+import { PropertyCard } from "./property-card/PropertyCard";
+import { usePropertyContext } from "@/shared/store/PropertyProvider";
+
 export function ListProperty() {
-  return <>List Property</>;
+  const { properties } = usePropertyContext();
+  if (properties.status === "loading") {
+    return <div>Loading...</div>;
+  } else if (properties.status === "error") {
+    return <div>Error...</div>;
+  }
+  return (
+    <div className="p-3 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {properties.data.map((item) => (
+        <Link to={`/view-property/${item.id}`} key={item.id}>
+          <PropertyCard
+            name={item.name}
+            financialRisk={item.totalFinancialRisk}
+            ratio={`${item.noRelevantRisks}/${item.noHandledRisks}`}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
