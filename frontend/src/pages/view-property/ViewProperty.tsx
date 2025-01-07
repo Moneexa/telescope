231967aae889317bbc3f735696227be81b04d71c
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-07 15:03:14 +0100

    added store state

--- frontend/src/pages/view-property/ViewProperty.tsx
+++ frontend/src/pages/view-property/ViewProperty.tsx
@@ -1,3 +1,34 @@
+import { usePropertyContext } from "@/shared/store/PropertyProvider";
+import { useParams } from "react-router-dom";
+
 export function ViewProperty() {
-  return <>This is ViewProperty</>;
+  const { id } = useParams();
+  const { properties } = usePropertyContext();
+  if (!id) {
+    return <div>Invalid ID</div>;
+  }
+  if (properties.status === "loading") {
+    return <div>Loading...</div>;
+  } else if (properties.status === "error") {
+    return <div>Error...</div>;
+  }
+
+  return (
+    <div className="flex justify-between items-center h-screen">
+      {properties.data.map((item) => {
+        return (
+          item.id === id && (
+            <div key={item.id} className="flex flex-col">
+              <div className="text-bold">{item.name}</div>
+              <div>Address: {item.address}</div>
+              <div>FinancialRisk: {item.totalFinancialRisk}</div>
+              <div>
+                Ratio: {item.noHandledRisks}/{item.noRelevantRisks}
+              </div>
+            </div>
+          )
+        );
+      })}
+    </div>
+  );
 }
