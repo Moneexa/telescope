461d6f082ca0dc782ea05fc17ed81082cc78e5c3
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-07 14:55:08 +0100

    installed google map + configured context API

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Routes from "./Routes.tsx";
+import { PropertyProvider } from "./shared/store/PropertyProvider.tsx";
+import { APIProvider } from "@vis.gl/react-google-maps";
+const apiKey = "AIzaSyCmhwxgkFS_R3sOr4LhiRDa8aIBGWd6vRM";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Routes />
-  </StrictMode>
+  <PropertyProvider>
+    <APIProvider
+      apiKey={apiKey}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <Routes />
+    </APIProvider>
+  </PropertyProvider>
 );
