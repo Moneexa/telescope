21a87fd181a15070dfc75ce409841828b7b9301e
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-09 15:56:02 +0100

    changed alignment

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,30 +1,64 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ListProperty } from "./list-property/ListProperty";
-import { ViewMap } from "./view-map/ViewMap";
-import { useEffect } from "react";
+import { ListProperty } from "./ListProperty";
 import { usePropertyContext } from "@/shared/store/PropertyProvider";
+import { MapProvider } from "@/shared/map-provider/Map";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
 
 export function Home() {
-  const { getList } = usePropertyContext();
-  useEffect(() => {
-    getList();
-  }, []);
+  const { properties } = usePropertyContext();
+
+  if (properties.status === "loading") {
+    return <div>Loading...</div>;
+  } else if (properties.status === "error") {
+    return <div>Error...</div>;
+  }
   return (
     <div className="flex flex-col p-5">
-      <Tabs defaultValue="list">
-        <div className="flex justify-center items-center">
-          <TabsList className="grid max-w-lg grid-cols-2">
-            <TabsTrigger value="list">Property List</TabsTrigger>
-            <TabsTrigger value="map">Property Map</TabsTrigger>
-          </TabsList>
+      <Tabs defaultValue="list" className="flex-1">
+        <div className="flex justify-center items-center py-5 relative">
+          <div className="flex justify-center items-center">
+            <TabsList className="grid max-w-lg grid-cols-2">
+              <TabsTrigger value="list">Property List</TabsTrigger>
+              <TabsTrigger value="map">Property Map</TabsTrigger>
+            </TabsList>
+          </div>
+          <Button className="absolute right-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <Link to="/add-property">Add Property</Link>
+          </Button>
         </div>
-        <TabsContent value="list" className="p-5">
+        <TabsContent value="list" className="py-5">
           <div className="text-bold">List of Properties</div>
-          <ListProperty />
+          {properties.data.length > 0 ? (
+            <ListProperty />
+          ) : (
+            <div className="py-5">
+              <Alert>
+                <Terminal className="h-4 w-4" />
+                <AlertTitle>Heads up!</AlertTitle>
+                <AlertDescription>
+                  You can add new properties to your app.
+                </AlertDescription>
+              </Alert>
+            </div>
+          )}
         </TabsContent>
-        <TabsContent value="map" className="p-5">
+        <TabsContent value="map" className="py-5">
           <div className="text-bold">List of Properties</div>
-          <ViewMap />
+          <div className="min-w-80 h-80">
+            <MapProvider
+              locations={
+                properties.data.length > 0
+                  ? properties?.data?.map((item) => ({
+                      key: item.id,
+                      location: item.coordinates,
+                    }))
+                  : [{ key: "1", location: { lat: 0, lng: 0 } }]
+              }
+            />
+          </div>
         </TabsContent>
       </Tabs>
     </div>
