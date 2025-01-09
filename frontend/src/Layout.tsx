28023334f994b4a6d5f43fda117547cbb65bf08a
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-09 15:46:51 +0100

    replaced fragment with div

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { TopBar } from "./shared/TopBar";
+import Breadcrumbs from "./shared/Breadcrumb";
 
 export function Layout() {
   return (
-    <>
+    <div>
       <TopBar />
+      <Breadcrumbs />
       <Outlet />
-    </>
+    </div>
   );
 }
