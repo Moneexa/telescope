4de416975228c1b3b7a5ed3772f92d13268dff81
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-09 15:47:15 +0100

    added react strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,18 @@ import "./index.css";
 import Routes from "./Routes.tsx";
 import { PropertyProvider } from "./shared/store/PropertyProvider.tsx";
 import { APIProvider } from "@vis.gl/react-google-maps";
-const apiKey = "AIzaSyCmhwxgkFS_R3sOr4LhiRDa8aIBGWd6vRM";
+import { StrictMode } from "react";
+export const apiKey = "AIzaSyCmhwxgkFS_R3sOr4LhiRDa8aIBGWd6vRM";
 
 createRoot(document.getElementById("root")!).render(
-  <PropertyProvider>
-    <APIProvider
-      apiKey={apiKey}
-      onLoad={() => console.log("Maps API has loaded.")}
-    >
-      <Routes />
-    </APIProvider>
-  </PropertyProvider>
+  <StrictMode>
+    <PropertyProvider>
+      <APIProvider
+        apiKey={apiKey}
+        onLoad={() => console.log("Maps API has loaded.")}
+      >
+        <Routes />
+      </APIProvider>
+    </PropertyProvider>
+  </StrictMode>
 );
