07be3abf394349e66c7f65e842c4e58133249e7b
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-11 13:30:10 +0100

    changed heading size

--- frontend/src/pages/add-property/AddProperty.tsx
+++ frontend/src/pages/add-property/AddProperty.tsx
@@ -3,7 +3,7 @@ import { PropertyForm } from "./PropertyForm";
 export function AddProperty() {
   return (
     <div className="flex flex-col p-5">
-      <div className="text-bold">Register Your Property...</div>
+      <div className="text-3xl">Register Your Property...</div>
       <PropertyForm />
     </div>
   );
