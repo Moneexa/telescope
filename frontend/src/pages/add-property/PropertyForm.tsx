9ed06b0cc8404b26daea785e28ca6335f36b040f
Moneexa <14besemsyed@seecs.edu.pk>
2025-01-12 11:04:57 +0100

    added AI icon to give value estimate

--- frontend/src/pages/add-property/PropertyForm.tsx
+++ frontend/src/pages/add-property/PropertyForm.tsx
@@ -9,6 +9,8 @@ import {
   formSchema,
   defaultValues,
 } from "@/pages/add-property/utils/formSchemaUtils";
+import { valueAssessmentFromAI } from "@/shared/apis/apis";
+
 import { MapProvider } from "@/shared/components/MapProvider";
 import { useState } from "react";
 import { Form } from "@/components/ui/form";
@@ -16,9 +18,22 @@ import { useNavigate } from "react-router-dom";
 import { usePropertyContext } from "@/shared/store/PropertyProvider";
 import { postPropertyItem } from "@/shared/apis/apis";
 import { getPinAddress } from "@/pages/add-property/utils/mapUtils";
+import { Response } from "@/types";
+import { SparklesIcon } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export function PropertyForm() {
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
+  const [propertyValue, setPropertyValue] = useState<Response<string>>();
+
   const { addPropertyItemInList } = usePropertyContext();
   const navigate = useNavigate();
   const form = useForm<FormSchema>({
@@ -55,6 +70,16 @@ export function PropertyForm() {
     console.log(address, city, zipCode);
   };
 
+  async function getAIEstimation() {
+    setPropertyValue({ status: "loading" });
+    const vals = await getPinAddress({ lat: location.lat, lng: location.lng });
+    if (vals) {
+      const risk = await valueAssessmentFromAI(vals);
+
+      setPropertyValue(risk);
+    }
+  }
+
   async function onSubmit(values: FormSchema) {
     console.log(values);
     const propertyResponse = await postPropertyItem(values);
@@ -83,7 +108,33 @@ export function PropertyForm() {
       <Form {...form}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-            <GenericForm fields={fieldMeta} control={control} />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <GenericForm fields={fieldMeta} control={control} />
+              {location.lat !== 0.0 && location.lng != 0.0 && (
+                <div className="col-span-1 flex items-end pb-2">
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <SparklesIcon onClick={() => getAIEstimation()} />
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogDescription>
+                          {propertyValue?.status === "success" &&
+                            propertyValue.data}
+                          {propertyValue?.status === "error" &&
+                            propertyValue.error}
+                          {propertyValue?.status === "loading" &&
+                            "... Waiting for AI for estimated value"}
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
+              )}
+            </div>
             <Button disabled={!isValid} type="submit">
               Submit
             </Button>
